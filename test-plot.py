de4e492adb7a36c27b5beb1800d95dcc97a735ad
sugita <sugita5019@gmail.com>
2020-01-08 10:41:36 +0800

    updated

--- test-plot.py
+++ test-plot.py
@@ -37,6 +37,11 @@ def cweno4(stencil):
     return uhalf
 
 
+def cweno4_int(stencil):
+    interpolate = 1.0
+
+    return interpolate
+
 print(plt.rcParams.get('figure.figsize'))
 pi = np.pi;
 print(pi)
@@ -53,11 +58,39 @@ x1 = np.linspace(0.0, 2.0*pi,N)
 x2 = np.linspace(0.0, 2.0*pi,N)
 stencil = np.ndarray((5), dtype=float)
 
+
 y1 = np.sin(x1)
 y2 = np.sin(x1)
 y3 = np.sin(x1)
 y4 = np.sin(x1)
 
+for i in range(N):
+    y1[i] = 0.0
+    if(x1[i] > 0.5*pi and x1[i] < pi):
+        y1[i] = 1.0
+        
+
+f = open("init.csv", "w")
+f.write("x, y\n")
+for i in range(N):
+    xg = str(x1[i])
+    yg = str(y1[i])
+    st0 = xg+","+yg+"\n"
+    f.write(st0)
+    
+f.close()
+
+'''
+plt.plot(x1, y1, 'ko') #analytical 
+plt.title('Function initial condition')
+plt.ylabel('y')
+plt.show()
+'''
+
+y2 = y1
+y3 = y1
+y4 = y1
+        
 imax = len(y1)
 dx = x1[1]-x1[0]
 
@@ -70,20 +103,12 @@ for i in range(2,imax-3):
 
 for i in range(2,imax-3):
     y3[i] = 0.5*(y1[i+1] + y1[i])
-    y2[i] = (-y1[i-2] + 7.0*y1[i] + 7.0*y1[i+1] -y1[i+2])/12.0 #central interpolation
+    y2[i] = (-y1[i-2] + 7.0*y1[i]
+             + 7.0*y1[i+1] -y1[i+2])/12.0 #central interpolation
     err1 = abs(y3[i]-y1[i])
     err2 = abs(y2[i]-y1[i])
     #print("Ave. based error=%f, 4th order based=%f"%(err1, err2))
 
-
-for i in range(5):
-    stencil[i] = float(i)
-    #print(stencil[i])
-
-#test(stencil)
-
-
-dummy = cweno4(stencil)
 
 for i in range(3, imax-4):
     '''
@@ -102,10 +127,8 @@ for i in range(3, imax-4):
     #interpolation
     y4[i] = cweno4(stencil)
 
-    error = abs( y4[i]-np.sin(x2[i]) )
-    print(error)
-
-    
+    #error = abs( y4[i]-np.sin(x2[i]) )
+    #print(error)
 
 
 '''    
@@ -130,8 +153,10 @@ blue_line = mlines.Line2D([], [], color='blue', marker='o',
 plt.legend(handles=[blue_line])
 '''
 
-plt.plot(x1, y1, 'ko-') #analytical 
-plt.plot(x2, y4, 'ro') #4th order CD interpolation
+
+#plt.plot(x1, y1, 'ko') #analytical 
+plt.plot(x2, y4, 'ro') #4th order central WENO interpolation
+#plt.plot(x2, y2, 'b^') #4th order CD interpolation
 #plt.plot(x2, y3, '^') #average [i,i+1]
 plt.title('Interpolation sample')
 plt.ylabel('y')
